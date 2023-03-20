d8fc3d3cefab2ae5d8865e525795df5568a29c7d
Jung Jinkyung <jinkyng7890@gmail.com>
2023-03-20 21:40:07 +0900

    fetch 성공

--- 프론트에서 서버에 데이터 요청하는 방법 연습/fetch/client/src/App.js
+++ 프론트에서 서버에 데이터 요청하는 방법 연습/fetch/client/src/App.js
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
+
 //fetch : 기본으로 제공하는 라이브러리
 //axios 
 
 function App() {
-  fetch('http://localhost:4000/api/todo')
-  .then((response) => response.json()) //json으로 한번 정제해 줘야 한다. 
-  .then((data) => console.log(data) )
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/todo')
+      .then((response) => response.json()) //json으로 한번 정제해 줘야 한다. 
+      .then((data) => setTodoList(data));
+  }, []);
+
   return (
     <div className="App">
       <h1>TODO LIST</h1>
+      {todoList.map((todo) => (
+        <div key={todo.id}>
+          <div>{todo.id}</div>
+          <div>{todo.text}</div>
+          <div>{todo.done ? 'done' : 'doing'}</div>
+        </div>
+      ))}
     </div>
   );
 }
