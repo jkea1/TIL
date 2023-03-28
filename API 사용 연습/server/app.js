3aed1a370ff6be91682ac31a4304675271664c92
Jung Jinkyung <jinkyng7890@gmail.com>
2023-03-28 23:48:12 +0900

    실제 db 이용해서 CRUD 기능 구현

--- API 사용 연습/server/app.js
+++ API 사용 연습/server/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test'
-});
+
+// connection이 제대로 완료가 되어야 db에서 data를 불러올 수 있다.
+//app.listen()에 넣어줘서 
+let connection;
 
 const database = [
   {id: 1, title: '글1'},
@@ -16,7 +14,7 @@ const database = [
 ];
 
 app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
 
 //http://localhost:3000
 //index.html 파일이 열린다. 
@@ -24,15 +22,23 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/database', (req, res) => {
-  res.send(database);
+app.get('/database', async (req, res) => {
+  const [rows, fields] = await connection.execute(`SELECT * FROM user`);
+  console.log('rows :', rows);
+  res.send(rows);
 });
 
 //특정 data를 가져오고 싶을때 
-app.get('/database/:id', (req, res) => {
+app.get('/database/:id', async(req, res) => {
   const id = req.params.id;
-  const data = database.find((el) => el.id === Number(id)); //el = element
-  res.send(data);
+  //sql
+  const [rows, fields] = await connection.execute(
+    `SELECT * FROM user WHERE id=?`,
+    [id]
+    );
+  console.log('rows :', rows);
+  /* const data = database.find((el) => el.id === Number(id)); //el = element */
+  res.send(rows[0]);
 })
 
 //REST API
@@ -57,30 +63,59 @@ app.get('/database/:id', (req, res) => {
 
 //데이터를 가져오는 방법 2 : req.body 이용하기 
 //글 추가하기 
-app.post('/database', (req, res) => {
-  const title = req.body.title;
-  database.push({
+app.post('/database', async (req, res) => {
+  //한 줄로 짤 수 있다. 
+  const {name, age} = req.body;
+  /* const name = req.body.name;
+  const age = req.body.age; */
+  //sql문
+  const [rows, fields] = await connection.execute(
+    `INSERT INTO user(name, age) VALUES(?, ?)`, 
+    [name, age]
+    );
+  /* database.push({
     id:database.length +1,
     title,
-  })
+  }) */
   res.send('값 추가가 정상적으로 완료되었습니다.');
 });
 
-//데이터 수정하는 방법
-app.put('/database', (req, res) => {
-  const id = req.body.id;
+//데이터 수정하는 방법(UPDATE)
+app.put('/database', async (req, res) => {
+  //sql문
+  const {id, name, age} = req.body;
+  /* const id = req.body.id;
   const title = req.body.title;
-  database[id-1].title = title;
+  database[id-1].title = title; */
+  const [rows, fields] = await connection.execute(
+    `UPDATE user SET name=?, age=? WHERE id=?`, 
+    [name, age, id]
+    );
   res.send('값 수정이 정상적으로 완료되었습니다.');
 });
 
 //데이터 삭제하는 법
-app.delete('/database/:id', (req, res) => {
+app.delete('/database/:id', async(req, res) => {
   const id = req.params.id; //delete method 같은 경우에는 req.body로 데이터를 전송하지 못하는 경우가 있기 때문에 url parameter로 전달한다. 
-  database.splice(id - 1, 1);
+  //sql
+  const [rows, fields] = await connection.execute(
+    `DELETE FROM user WHERE id=?`, 
+    [id]
+    );
+  /* database.splice(id - 1, 1); */
   res.send('값 삭제가 정상적으로 완료되었습니다.');
 });
 
-app.listen(3000, () => {
+//서버를 띄울때 비동기적으로 connection이 연결된후에 실행할 수 있도록 한다. 
+//서버가 db에 connection이 되어야 db에서 정보를 가져올 수 있다. 안되면 콜백함수 에러가 뜬다.  
+app.listen(3000, async() => {
+  // create the connection to database
+  connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'myapp',
+    password: 'root',
+  });
+  
   console.log('server on!');
 });
