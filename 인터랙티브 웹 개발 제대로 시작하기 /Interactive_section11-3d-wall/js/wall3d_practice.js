4c4078f46daabd408dab4a2e2cd05130a751ab34
jkea1 <jinkyng7890@gmail.com>
2023-05-29 17:22:51 +0900

    스크롤 수치화 하기

---
+++ 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
@@ -0,0 +1,9 @@
+(function() {
+  const houseElem = document.querySelector('.house');
+  window.addEventListener('scroll', function() {
+    //얼마큼 스크롤 했지는지 수치화 할 수 있어야 한다. 
+    console.log(pageYOffset);
+
+
+  })
+})();
