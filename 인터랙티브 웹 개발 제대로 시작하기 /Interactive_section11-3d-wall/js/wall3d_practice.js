e7d12c512198e2c88f4e12eef7cc47e364d3e54d
jkea1 <jinkyng7890@gmail.com>
2023-05-31 15:33:33 +0900

    resizeHandler 추가해서 resize 대응하기

--- 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
+++ 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
@@ -1,9 +1,18 @@
+
 (function() {
   const houseElem = document.querySelector('.house');
   let maxScrollValue = document.body.offsetHeight - window.innerHeight;
-      //얼마큼 스크롤 했지는지 수치화 할 수 있어야 한다. 
+  
+  //window size(창 사이즈)가 바뀔때 마다 maxsizevalue 값이 갱신되야 한다. 
+  function resizeHandler () {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+  
+  //얼마큼 스크롤 했지는지 수치화 할 수 있어야 한다. 
   window.addEventListener('scroll', function() {
     const zMove = pageYOffset / maxScrollValue * 980 - 490; /* 0 ~ 1000 으로 세팅된다.  */
     houseElem.style.transform = 'translateZ(' + zMove +'vw)';
   });
+
+  window.addEventListener('resize', resizeHandler);
 })();
