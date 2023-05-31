cbb2e56bf6c1fc5a4394562fd60392560fe67ffa
jkea1 <jinkyng7890@gmail.com>
2023-05-31 14:46:52 +0900

    스크롤 수치화 & scroll event 추가

--- 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
+++ 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
@@ -1,9 +1,9 @@
 (function() {
   const houseElem = document.querySelector('.house');
+  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+      //얼마큼 스크롤 했지는지 수치화 할 수 있어야 한다. 
   window.addEventListener('scroll', function() {
-    //얼마큼 스크롤 했지는지 수치화 할 수 있어야 한다. 
-    console.log(pageYOffset);
-
-
-  })
+    const zMove = pageYOffset / maxScrollValue * 980 - 490; /* 0 ~ 1000 으로 세팅된다.  */
+    houseElem.style.transform = 'translateZ(' + zMove +'vw)';
+  });
 })();
