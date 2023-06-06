fae92308f75e18ae63fc93ed29e424503bed5667
jkea1 <jinkyng7890@gmail.com>
2023-06-06 12:45:11 +0900

    스크롤 정도에 따른 progress bar 만들기

--- 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
+++ 인터랙티브 웹 개발 제대로 시작하기 /Interactive_section11-3d-wall/js/wall3d_practice.js
@@ -1,6 +1,7 @@
 
 (function() {
   const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar');
   let maxScrollValue;
   
   //window size(창 사이즈)가 바뀔때 마다 maxsizevalue 값이 갱신되야 한다. 
@@ -10,13 +11,16 @@
   
   //얼마큼 스크롤 했지는지 수치화 할 수 있어야 한다. 
   window.addEventListener('scroll', function() {
-    const zMove = pageYOffset / maxScrollValue * 980 - 490; /* 0 ~ 1000 으로 세팅된다.  */
+    const scrollPer = pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490; /* 0 ~ 1000 으로 세팅된다.  */
     houseElem.style.transform = 'translateZ(' + zMove +'vw)';
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + "%";
   });
 
-  window.addEventListener('resize', resizeHandler);
   //처음에 문서가 로드 되면 resizeHandler를 한 번 실행 해 주어 maxScrollValue 를 초기화 해준다. 
-  
+  window.addEventListener('resize', resizeHandler);
   //resizehandler() 함수를 실행 해 주지 않다면 maxScrollValue 값이 없어 -> transform이 적용되지 않는다 -> resize event를 해 주지 않는 이상 스크롤 event가 일어나지 않는다. 
   resizeHandler();
 })();
