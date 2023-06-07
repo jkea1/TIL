85f5f3272449443fbe130e6472737bc7a33f550f
jkea1 <jinkyng7890@gmail.com>
2023-06-07 16:21:02 +0900

    해시테이블 베스트 앨범 문제

---
+++ 프로그래머스 데브코스 4기/day5/해시 테이블 코테 문제 /베스트 앨범.js
@@ -0,0 +1,24 @@
+//1. 같은 장르끼리 묶어야 한다. 
+//2. 묶인 노래들을 재생 순으로 정렬을 해야 한다.
+//3. 노래를 2개까지 자르는 잡업을 해야한다. 
+
+//핵심 키워드 "묶는 것", "정렬"
+
+function solution(genres, plays) {
+  const genreMap = new Map();
+  genres
+    .map((genre, index) => [genre, plays[index]])
+    .forEach(([genre, play], index) => {
+      const data = genreMap.get(genre) || {total: 0, songs: []};
+      genreMap.set(genre, {
+          total: data.total + play,
+          songs: [...data.songs, { play, index }]
+            .sort((a,b) => b.play - a.play) 
+            .slice(0,2)
+      })
+    })
+    return  [...genreMap.entries()]
+            .sort((a, b) => b[1].total - a[1].total)
+            .flatMap(item => item[1].songs) //map을 쓰면 리스트안에 리스트가 들어가 버린다.
+            .map(song => song.index)
+} 
