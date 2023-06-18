22526aa7711ea1a46493b6624be2dfd48d5e47f8
jkea1 <jinkyng7890@gmail.com>
2023-06-18 16:58:20 +0900

    선언형 방식 컴포넌트 생성

--- 프로그래머스 데브코스 4기/day11/명령형 프로그래밍과 선언적인 프로그래밍 방식의 이해/example3.js
+++ 프로그래머스 데브코스 4기/day11/명령형 프로그래밍과 선언적인 프로그래밍 방식의 이해/example3.js
@@ -1,2 +1,32 @@
 //토글 버튼 만들기
 //버튼 3개 만들기
+const $button1 = document.createElement("button");
+$button1.textContent = "Button1"; //아직 메모리 상에서만 생성이 된 것이다.
+
+const $button2 = document.createElement("button");
+$button2.textContent = "Button2";
+
+const $button3 = document.createElement("button");
+$button3.textContent = "Button3";
+
+//만든 버튼을 화면에 그린다.
+const $main = document.querySelector("#app");
+$main.appendChild($button1);
+$main.appendChild($button2);
+$main.appendChild($button3);
+
+const toggleButton = ($button) => {
+  if ($button.style.textDecoration === "line-through") {
+    $button.style.textDecoration = "none";
+  } else {
+    $button.style.textDecoration = "line-through";
+  }
+};
+
+//버튼을 클릭하면 삭선이 그어진다.
+document.querySelectorAll("button").forEach(($button) => {
+  $button.addEventListener("click", (e) => {
+    const { target } = e;
+    toggleButton(target);
+  });
+});
---
+++ 프로그래머스 데브코스 4기/day11/명령형 프로그래밍과 선언적인 프로그래밍 방식의 이해/example3_version2.js
@@ -0,0 +1,172 @@
+//버튼 생성 & 토글 기능 추가 함수
+
+// function ToggleButton({ $target, text }) {
+//   const $button = document.createElement("button");
+//   let isInit = false;
+
+//   console.log(this);
+//   this.render = () => {
+//     $button.textContent = text;
+
+//     if (!isInit) {
+//       $target.appendChild($button);
+//       $button.addEventListener("click", () => {
+//         if ($button.style.textDecoration === "line-through") {
+//           $button.style.textDecoration = "none";
+//         } else {
+//           $button.style.textDecoration = "line-through";
+//         }
+//       });
+//       isInit = true;
+//     }
+//   };
+
+//   this.render();
+// }
+
+// const $app = document.querySelector("#app");
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "Button1",
+// });
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "Button2",
+// });
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "Button3",
+// });
+
+//기능 추가하기1 : 3번 클릭할 때마다 alert 띄우기
+//기능 추가하기2 : ToggleButton 외에 5초 뒤에 자동으로 토글 되는 버튼 만들기
+//기능 추가하기3 : ButtonGroup 만들기
+
+function TimerButton({ $target, text, timer = 3000, onClick }) {
+  const button = new ToggleButton({
+    $target,
+    text,
+    onClick: () => {
+      setTimeout(() => {
+        button.setState({
+          ...button.state,
+          toggled: !button.state.toggled,
+        });
+      }, timer);
+    },
+  });
+}
+
+function ToggleButton({ $target, text, onClick }) {
+  const $button = document.createElement("button");
+  $target.appendChild($button);
+
+  this.state = {
+    clickCount: 0,
+    toggled: false,
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $button.textContent = text;
+    $button.style.textDecoration = this.state.toggled ? "line-through" : "none";
+  };
+
+  $button.addEventListener("click", () => {
+    this.setState({
+      clickCount: this.state.clickCount + 1,
+      toggled: !this.state.toggled,
+    });
+
+    if (onClick) {
+      onClick(this.state.clickCount);
+    }
+  });
+
+  this.render();
+}
+
+function ButtonGroup({ $target, buttons }) {
+  const $group = document.createElement("div");
+  //render 함수가 여러번 불릴 수 도 있으므로 안정장치를 만들어 둔 것임
+  let isInit = false;
+
+  //render 함수 정의 함
+  this.render = () => {
+    if (!isInit) {
+      buttons.forEach(({ type, ...props }) => {
+        console.log(type, props);
+        if (type === "toggle") {
+          new ToggleButton({ $target: $group, ...props });
+        } else if (type === "timer") {
+          new TimerButton({ $target: $group, ...props });
+        }
+      });
+      $target.appendChild($group);
+      isInit = true;
+    }
+  };
+  this.render();
+}
+
+const $app = document.querySelector("#app");
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "버튼1",
+// });
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "버튼2",
+// });
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "버튼3",
+// });
+
+// new ToggleButton({
+//   $target: $app,
+//   text: "버튼4",
+// });
+
+// new TimerButton({
+//   $target: $app,
+//   text: "3초 뒤에 자동으로!",
+// });
+
+// new TimerButton({
+//   $target: $app,
+//   text: "10초 뒤에 자동으로!",
+//   timer: 1000 * 10,
+// });
+
+//명령적인 방법보다 이렇게 선언적인 방버을 써야한다.
+//이렇게 바닐라js 방법으로만으로만 이렇게 구현이 되네!!
+//⭐️ 이런식으로 하면 재사용성도 높아지고, 코드를 공유하기도 쉽고, 파리미터에 의해 들어온 값에 의해서만 실행되기 때문에 격리된 구조이기 때문에 외부의 개입이 최소화 되고 독립적으로 돌아갈 수 있다. 그리고 상태를 기반으로 추상화 되어 있는  UI이다.
+new ButtonGroup({
+  $target: $app,
+  buttons: [
+    {
+      type: "toggle",
+      text: "토글 버튼",
+    },
+    {
+      type: "toggle",
+      text: "토글 버튼",
+    },
+    {
+      type: "timer",
+      text: "타이머!",
+      timer: 3000,
+    },
+  ],
+});
