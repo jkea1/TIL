69cb4ace2a03a8240623ab224f45ecb8b5dfe4a1
jkea1 <jinkyng7890@gmail.com>
2023-07-31 00:41:56 +0900

    재귀 알고리즘 공부

---
+++ coding-test-practice/자바스크립트 알고리즘 문제풀이 입문(코딩테스트 대비)/섹션 8.  재귀함수와 완전탐색(DFS:깊이우선탐색) /재귀함수와 스택프레임/index.js
@@ -0,0 +1,21 @@
+// DFS: 깊이 우선 탐색
+// L: level
+// 재귀함수 : 자기가 자신을 호출하는 함수
+// 함수가 호출되면 (콜)스택에 함수정보가 들어간다.
+// 함수의 매개변수, 지역변수, 복귀주소(함수코드가 실행된 위치)를 저장한다. = stact frame = 스택에 들어가는 하나의 아이템
+//  스택 -> [DFS(3), DFS(2), DFS(1)] -> [DFS(3), DFS(2)] -> [DFS(3)] -> []
+function solution(n) {
+  function DFS(L) {
+    if (L === 0) {
+      return;
+    } else {
+      // console.log(L);
+      DFS(L - 1);
+      console.log("재귀", L);
+    }
+  }
+
+  DFS(n);
+}
+
+solution(3);
