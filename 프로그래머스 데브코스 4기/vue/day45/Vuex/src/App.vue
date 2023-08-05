81ed8c752447670898f951828d747a83daac5467
jkea1 <jinkyng7890@gmail.com>
2023-08-05 16:49:09 +0900

    chore

---
+++ 프로그래머스 데브코스 4기/vue/day45/Vuex/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+}
+</script>
+
+<style>
+h1 {
+  color:rgb(80, 81, 80);
+}
+</style>
+
---
+++ 프로그래머스 데브코스 4기/vue/day45/Vuex/src/components/Hello.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style scoped lang="scss">
+$color: rgb(43, 46, 45);
+h1 {
+  color: $color;
+}
+</style>
