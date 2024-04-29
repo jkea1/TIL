0f819482f2b27f41734e51014959f4b6d0eca3ee
jkea1 <jinkyng7890@gmail.com>
2024-04-29 16:20:11 +0900

    feat: 스크롤 애니메이션 적용하기

--- 스크롤 애니메이션/scroll_reveal_animation.js
+++ 스크롤 애니메이션/scroll_reveal_animation.js
@@ -3,26 +3,28 @@ const scrollElements = document.querySelectorAll('.js-scroll')
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top
 
+  // viewport 기준 element top이 보이기 시작할때는 감지한다.
   return (
     elementTop <=
     (window.innerHeight || document.documentElement.clientHeight / dividend) // dividend // 4/5 배
   )
 }
 
+// el가 viewport 아래로 사라질때를 감지한다.
 const elementOutofView = (el) => {
   const elementTop = el.getBoundingClientRect().top
 
   return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    elementTop > window.innerHeight || document.documentElement.clientHeight
   )
 }
 
-const displayScrollElement = (element) => {
-  element.classList.add('scrolled')
+const displayScrollElement = (el) => {
+  el.classList.add('scrolled')
 }
 
-const hideScrollElement = (element) => {
-  element.classList.remove('scrolled')
+const hideScrollElement = (el) => {
+  el.classList.remove('scrolled')
 }
 
 const handleScrollAnimation = () => {
